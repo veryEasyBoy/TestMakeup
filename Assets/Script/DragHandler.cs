47231a2356f75bebd181239b01a680819db73c5a
EasyBoy <soldersen@mail.ru>
2025-07-24 21:36:51 +0300

    Update 2

--- Assets/Script/DragHandler.cs
+++ Assets/Script/DragHandler.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class DragHandler : MonoBehaviour, IDragHandler
 {
-    public RectTransform draggableTransform;
+    [SerializeField] private RectTransform draggableTransform;
+
+    // Задает новую позицию
     public void OnDrag(PointerEventData eventData)
     {
-        // Рассчитать новую позицию на основе дельты перетаскивания  
         draggableTransform.anchoredPosition += eventData.delta;
     }
 }  
