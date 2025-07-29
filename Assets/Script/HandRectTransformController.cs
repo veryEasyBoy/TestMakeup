f092ef931c6858be829df42616674d5d1b9fc71c
EasyBoy <soldersen@mail.ru>
2025-07-30 00:55:51 +0300

    Update 3
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity
    #	Assets/Script/AddTowardsTarget.cs
    #	Assets/Script/HandRectTransformController.cs

--- Assets/Script/HandRectTransformController.cs
+++ Assets/Script/HandRectTransformController.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HandRectTransformController : MonoBehaviour
@@ -8,25 +9,32 @@ public class HandRectTransformController : MonoBehaviour
     [Header("Time to achieve the goal")]
     [SerializeField] private float duration;
     private TweenRectTransformComponent rectTransformComponent;
+    [SerializeField] private RectTransform addTarget;
     private RectTransform myRectTransform;
+    [SerializeField] private CommandInvoker commandInvoker;
+    private AddTowardsTarget addTowardsTarget;
+    [SerializeField] private List<Vector3> previousPosition;
+    [SerializeField] private RectTransform towardsObject;
     private void Awake()
     {
         myRectTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<RectTransform>();
         rectTransformComponent = new DefaultRectTransformComponent(myRectTransform);
+        addTowardsTarget = new AddTowardsTarget(rectTransformComponent, myRectTransform, previousPosition, addTarget, towardsObject);
     }
     // Двигает руку к таргету
     private Tween MovingTowards()
     {
-        towardsTarget.position = myRectTransform.position;
-        towardsTarget.SetParent(myRectTransform);
         return rectTransformComponent.GetTweenRectTransform(towardsTarget, duration);
     }
     // Двигает руку к таргету, затем двигает ее к добавленному таргету
-    private Tween AddTargetMovingTowards(RectTransform addTarget)
+    private Tween AddTargetMovingTowards()
     {
-        rectTransformComponent = new AddTowardsTarget(rectTransformComponent,myRectTransform, addTarget);
-        return rectTransformComponent.GetTweenRectTransform(towardsTarget, duration);
+        return addTowardsTarget.Execute(duration);
     }
-    public void StartAddMovingTowards(RectTransform addTarget) => AddTargetMovingTowards(addTarget);
+    public void Undo()
+    {
+        addTowardsTarget.Undo(duration);
+    }
+    public void StartAddMovingTowards() => AddTargetMovingTowards();
     public void StartMovingTowards() => MovingTowards();
 }
